d41e7f8f03b1a766ebd659a7eaacf461fb858621
yujhenchen <yujhen.chen93@gmail.com>
2024-07-17 20:45:53 +0800

    feat: setup socket.io and listen to events

--- server.ts
+++ server.ts
@@ -1,12 +1,25 @@
 import express from "express";
+import { Server } from 'socket.io';
+import http from 'http';
 
 const app = express();
 const port = 8080;
+
+// create a HTTP server object
+const server = http.createServer(app);
+
+// initialize a new instance of socket.io
+const io = new Server(server);
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
-app.listen(port, () => {
+// listen on the connection event for incoming sockets
+io.on('connection', (socket) => {
+    console.log('a user connected');
+});
+
+server.listen(port, () => {
     console.log(`Listening on port ${port}...`);
 });
