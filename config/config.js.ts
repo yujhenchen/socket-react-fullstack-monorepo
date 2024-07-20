90f5adccb2836f5a04b8b53df7f5adbe859db32f
yujhenchen <yujhen.chen93@gmail.com>
2024-07-20 13:54:28 +0800

    feat: add basic structure of config file

--- config/config.js.ts
+++ config/config.js.ts
@@ -1,3 +1,12 @@
-export const config = {
+import { z } from 'zod';
 
+const envVarsSchema = z.object({
+    SERVER_PORT: z.number().default(3000)
+});
+
+// TODO: use the right way to validate schema and handle errors
+const envVars = envVarsSchema.parse(process.env);
+
+export const config = {
+    port: envVars.SERVER_PORT,
 };
