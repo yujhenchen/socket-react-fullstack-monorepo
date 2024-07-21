124631e9aef877c060fa7cf7f5e82cc7ef8ad733
yujhenchen <yujhen.chen93@gmail.com>
2024-07-21 15:29:39 +0800

    feat: add env variables

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = import.meta.env.NODE_ENV === 'production' ? 'TODO: add production url' : 'http://localhost:4000';
+const URL = import.meta.env.NODE_ENV === 'production' ? 'TODO: add production url' : `${import.meta.env.SERVER_HOST}:${import.meta.env.SERVER_PORT}`;
 
 export const socket = io(URL);
