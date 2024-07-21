d18b30b0a99ff23b925d3bbc202f1167d96f448e
yujhenchen <yujhen.chen93@gmail.com>
2024-07-21 16:34:11 +0800

    feat: add client basic socket connection control and state check

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+// import "./App.css";
+import { socket } from "./socket";
+import { ConnectionState } from "./components/ConnectionState";
+import { ConnectionManager } from "./components/ConnectionManager";
+// import { Events } from "./components/Events";
+// import { MyForm } from "./components/MyForm";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isConnected, setIsConnected] = useState<boolean>(socket.connected);
+  // const [fooEvents, setFooEvents] = useState<string[]>([]);
+
+  useEffect(() => {
+    function onConnect() {
+      setIsConnected(true);
+    }
+
+    function onDisconnect() {
+      setIsConnected(false);
+    }
+
+    // function onFooEvent(value: string) {
+    //   setFooEvents((previous) => [...previous, value]);
+    // }
+
+    socket.on("connect", onConnect);
+    socket.on("disconnect", onDisconnect);
+    // socket.on("foo", onFooEvent);
+
+    return () => {
+      socket.off("connect", onConnect);
+      socket.off("disconnect", onDisconnect);
+      // socket.off("foo", onFooEvent);
+    };
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <ConnectionState isConnected={isConnected} />
+      {/* <Events events={fooEvents} /> */}
+      <ConnectionManager />
+      {/* <MyForm /> */}
+    </div>
+  );
 }
 
-export default App
+export default App;
