84b1f085646d685e58124a3c64d9af602d26a238
yujhenchen <yujhen.chen93@gmail.com>
2024-07-21 20:09:03 +0800

    feat: add MyForm and handle on submit

---
+++ frontend/src/components/MyForm.tsx
@@ -0,0 +1,34 @@
+import { FormEvent, useState } from "react";
+import { socket } from "../socket";
+
+export default function MyForm() {
+  const [value, setValue] = useState<string>("");
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [errors, setErrors] = useState<Error | null>(null);
+
+  function onSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    setIsLoading(true);
+
+    try {
+      socket.timeout(5000).emit("create-something", value, () => {
+        setIsLoading(false);
+      });
+    } catch (error) {
+      setIsLoading(false);
+      if (error instanceof Error) setErrors(error);
+      console.error(error);
+    }
+  }
+
+  if (errors) return null;
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input type="text" onChange={(e) => setValue(e.target.value)} />
+      <button type="submit" disabled={isLoading}>
+        Submit
+      </button>
+    </form>
+  );
+}
