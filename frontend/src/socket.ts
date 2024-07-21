f42410f25c18f937c4ae3adf2b7a9901fa01b8e0
yujhenchen <yujhen.chen93@gmail.com>
2024-07-21 16:32:35 +0800

    fix: incorrect vite env variable names

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -1,6 +1,8 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = import.meta.env.NODE_ENV === undefined ? 'TODO: add production url' : `${import.meta.env.SERVER_HOST}:${import.meta.env.SERVER_PORT}`;
+const URL = import.meta.env.VITE_NODE_ENV === undefined ? 'TODO: add production url' : import.meta.env.VITE_SERVER_URL;
 
-export const socket = io(URL);
+export const socket = io(URL, {
+    // withCredentials: true,
+});
