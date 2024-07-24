40d297f11013d25d332ab34a944e054117ff513f
yujhenchen <yujhen.chen93@gmail.com>
2024-07-24 16:00:20 +0800

    fix: fix unknown .ts file error, temp remove not need files, fix libs in package.json

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,45 +1,13 @@
-import express from "express";
-import { Server } from 'socket.io';
-import http from 'http';
-import { env } from "./config/env";
-import cors from 'cors';
+import express, { Express, Request, Response } from "express";
 
-const app = express();
-const port = env.port;
 
-app.use(cors());
+const app: Express = express();
+const port = 3000;
 
-// create a HTTP server object
-const server = http.createServer(app);
-
-// initialize a new instance of socket.io
-const io = new Server(server, {
-    cors: {
-        origin: env.clientUrl,
-        // credentials: true
-    }
+app.get("/", (req: Request, res: Response) => {
+    res.send("Express + TypeScript Server");
 });
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
-
-// listen on the connection event for incoming sockets
-io.on('connection', (socket) => {
-    console.log('a user connected', socket.id);
-
-    socket.on("send_msg", (msg: string) => {
-        console.log(`msg: ${msg}`);
-        // TODO: study the difference between `socket.broadcast.emit` and `io.emit`
-        io.emit('receive_msg', msg);    // Broadcast the message to all connected clients (including the sender)
-        // socket.broadcast.emit('receive_msg', msg);    // Broadcast the message to all connected clients (except the sender)
-    });
-
-    socket.on('disconnect', () => {
-        console.log('A user disconnected:', socket.id)
-    })
-});
-
-server.listen(port, () => {
-    console.log(`Listening on port ${port}...`);
+app.listen(port, () => {
+    console.log(`[server]: Server is running at http://localhost:${port}`);
 });
