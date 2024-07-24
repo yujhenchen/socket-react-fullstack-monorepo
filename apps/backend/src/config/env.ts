122c90debd85f4537de235a2c6c6ef86eace553c
yujhenchen <yujhen.chen93@gmail.com>
2024-07-24 22:51:01 +0800

    feat: use express.js socket project files

---
+++ apps/backend/src/config/env.ts
@@ -0,0 +1,23 @@
+import { z } from 'zod';
+
+const envSchema = z.object({
+    env: z.enum(['development', 'production', 'test']).default('development'),
+    port: z.string().default('3000'),
+    clientUrl: z.string().default('http://localhost:5173')
+});
+
+const envServer = envSchema.safeParse({
+    env: process.env.NODE_ENV,
+    port: process.env.SERVER_PORT,
+    clientUrl: process.env.CLIENT_URL
+});
+
+if (!envServer.success) {
+    console.error(envServer.error.issues);
+    throw new Error('There is an error with the server environment variables');
+    // process.exit(1);  // won't reach here
+}
+
+export const env = envServer.data;
+
+export type Environment = z.infer<typeof envSchema>;
