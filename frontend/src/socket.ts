9338579eb5b98a47cc22eb4c8a9ffd1391bc9bc5
Turbobot <turbobot@vercel.com>
2024-07-24 11:05:59 +0800

    feat(create-turbo): apply official-starter transform

--- frontend/src/socket.ts
+++
@@ -1,8 +0,0 @@
-import { io } from 'socket.io-client';
-
-// "undefined" means the URL will be computed from the `window.location` object
-const URL = import.meta.env.VITE_NODE_ENV === 'production' ? undefined : import.meta.env.VITE_SERVER_URL;
-
-export const socket = io(URL, {
-    // withCredentials: true,
-});
