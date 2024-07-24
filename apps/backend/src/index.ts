8aa8a1ab667b2f90b69219ffe2d3d5c11d58b0d5
yujhenchen <yujhen.chen93@gmail.com>
2024-07-24 14:16:43 +0800

    feat: add backend files

---
+++ apps/backend/src/index.ts
@@ -0,0 +1,45 @@
+import express from "express";
+import { Server } from 'socket.io';
+import http from 'http';
+import { env } from "./config/env";
+import cors from 'cors';
+
+const app = express();
+const port = env.port;
+
+app.use(cors());
+
+// create a HTTP server object
+const server = http.createServer(app);
+
+// initialize a new instance of socket.io
+const io = new Server(server, {
+    cors: {
+        origin: env.clientUrl,
+        // credentials: true
+    }
+});
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+// listen on the connection event for incoming sockets
+io.on('connection', (socket) => {
+    console.log('a user connected', socket.id);
+
+    socket.on("send_msg", (msg: string) => {
+        console.log(`msg: ${msg}`);
+        // TODO: study the difference between `socket.broadcast.emit` and `io.emit`
+        io.emit('receive_msg', msg);    // Broadcast the message to all connected clients (including the sender)
+        // socket.broadcast.emit('receive_msg', msg);    // Broadcast the message to all connected clients (except the sender)
+    });
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected:', socket.id)
+    })
+});
+
+server.listen(port, () => {
+    console.log(`Listening on port ${port}...`);
+});
