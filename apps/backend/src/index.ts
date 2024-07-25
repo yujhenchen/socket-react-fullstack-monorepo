baf700740de0dc77d7094127096e72b96bed5403
yujhenchen <yujhen.chen93@gmail.com>
2024-07-25 16:04:49 +0800

    feat: make log clear, handle dropdown

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -29,10 +29,23 @@ io.on('connection', (socket) => {
     console.log('a user connected', socket.id);
 
     socket.on("send_msg", (msg: string) => {
-        console.log(`msg: ${msg}`);
+        console.log(`socket.id: ${socket.id}, msg: ${msg}`);
         // TODO: study the difference between `socket.broadcast.emit` and `io.emit`
         io.emit('receive_msg', msg);    // Broadcast the message to all connected clients (including the sender)
         // socket.broadcast.emit('receive_msg', msg);    // Broadcast the message to all connected clients (except the sender)
+    });
+
+    // TODO: how to handle boolean, client side HTML select component cannot handle expanding or not
+    // socket.on("dropdown_is_expanded", (isExpanded: boolean) => { 
+    //     console.log(`socket.id: ${socket.id}, isExpanded: ${isExpanded}`);
+
+    //     io.emit('receive_dropdown_is_expanded', isExpanded);
+    // });
+
+    socket.on("dropdown_selected_value", (value: string) => {
+        console.log(`socket.id: ${socket.id}, value: ${value}`);
+
+        io.emit('receive_dropdown_selected_value', value);
     });
 
     socket.on('disconnect', () => {
--- apps/web/src/components/AppDropdown.tsx
+++ apps/web/src/components/AppDropdown.tsx
@@ -1,24 +1,33 @@
-import { Dropdown } from "flowbite-react";
+import { ChangeEvent, useEffect, useState } from "react";
+import { socket } from "../socket";
 
 interface Props {
   items: string[];
 }
 
 export function AppDropdown({ items }: Props) {
-  function handleOnClick() {}
+  const [selectedValue, setSelectedValue] = useState<string>("");
 
-  function handleOonChange() {}
+  useEffect(() => {
+    socket.on("receive_dropdown_selected_value", (value: string) =>
+      setSelectedValue(value)
+    );
+  }, [socket]);
+
+  function handleOnChange(event: ChangeEvent<HTMLSelectElement>) {
+    setSelectedValue(event.target.value);
+    try {
+      socket.emit("dropdown_selected_value", event.target.value);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
   return (
-    <Dropdown
-      label="Dropdown button"
-      dismissOnClick={false}
-      onClick={handleOnClick}
-      onChange={handleOonChange}
-    >
+    <select value={selectedValue} onChange={handleOnChange}>
       {items.map((item, index) => (
-        <Dropdown.Item key={index}>{item}</Dropdown.Item>
+        <option key={index}>{item}</option>
       ))}
-    </Dropdown>
+    </select>
   );
 }
