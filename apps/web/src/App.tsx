9fa87674f4790fdbc3b959004129d10f6ea8a73c
yujhenchen <yujhen.chen93@gmail.com>
2024-07-27 15:19:37 +0800

    feat: comment out not handle error type

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -32,11 +32,13 @@ function App() {
       // the reason of the error, for example "xhr poll error"
       console.log(err.message);
 
+      /*
       // some additional description, for example the status code of the initial HTTP response
       console.log(err.description);
 
       // some additional context, for example the XMLHttpRequest object
       console.log(err.context);
+      */
     });
 
     return () => {
