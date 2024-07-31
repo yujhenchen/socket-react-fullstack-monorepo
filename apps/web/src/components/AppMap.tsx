bb07ccd6d6726c34fdb49731bd8801fd0964d6c8
yujhenchen <yujhen.chen93@gmail.com>
2024-07-31 22:55:52 +0800

    feat: add map on click event

--- apps/web/src/components/AppMap.tsx
+++ apps/web/src/components/AppMap.tsx
@@ -1,11 +1,36 @@
-import { APIProvider, Map, Marker } from "@vis.gl/react-google-maps";
+import {
+  APIProvider,
+  Map,
+  MapMouseEvent,
+  Marker,
+} from "@vis.gl/react-google-maps";
+import { useEffect, useState } from "react";
+import { socket } from "../socket";
+
+const defaultPosition = { lat: 43.2203, lng: 142.8635 };
+
+type Position = typeof defaultPosition;
 
 export function AppMap() {
-  const position = { lat: 43.2203, lng: 142.8635 };
+  const [position, setPosition] = useState(defaultPosition);
+
+  useEffect(() => {
+    socket.on("receive_map_position", (value: Position) => setPosition(value));
+  }, [socket]);
+
+  function handleOnClick(event: MapMouseEvent) {
+    console.log(event.detail.latLng);
+    if (event.detail.latLng) setPosition(event.detail.latLng);
+    try {
+      socket.emit("map_position", event.detail.latLng);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
   return (
     <APIProvider apiKey={import.meta.env.VITE_MAP_API_KEY}>
-      <Map defaultCenter={position} defaultZoom={10}>
+      <Map defaultCenter={position} defaultZoom={10} onClick={handleOnClick}>
         <Marker position={position} />
       </Map>
     </APIProvider>
