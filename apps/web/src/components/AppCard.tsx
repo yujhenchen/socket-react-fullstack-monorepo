844d2bc84d915d5866d8c77a569741b854d79218
yujhenchen <yujhen.chen93@gmail.com>
2024-08-01 23:19:02 +0800

    feat: add styleClassName property

--- apps/web/src/components/AppCard.tsx
+++ apps/web/src/components/AppCard.tsx
@@ -1,13 +1,15 @@
 import { Card } from "flowbite-react";
 import { ReactNode } from "react";
+import { cn } from "../utils/cn";
 
 interface Props {
   children: ReactNode;
+  styleClassName?: string;
 }
 
-export function AppCard({ children }: Props) {
+export function AppCard({ children, styleClassName }: Props) {
   return (
-    <Card href="#" className="min-w-fit m-h-fit">
+    <Card href="#" className={cn("min-w-fit m-h-fit", styleClassName ?? "")}>
       {children}
     </Card>
   );
