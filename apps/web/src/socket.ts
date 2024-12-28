70a20946b4ba97b03198f2cddb46fc79113cd7fa
yujhenchen <yujhen.chen93@gmail.com>
2024-12-28 17:05:16 +0800

    feat: add missing ts node, explicit type annotation

--- apps/web/src/socket.ts
+++ apps/web/src/socket.ts
@@ -1,8 +1,8 @@
-import { io } from 'socket.io-client';
+import { io, Socket } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
 const URL = import.meta.env.VITE_NODE_ENV === 'production' ? undefined : import.meta.env.VITE_SERVER_URL;
 
-export const socket = io(URL, {
+export const socket: Socket = io(URL, {
     // withCredentials: true,
 });
